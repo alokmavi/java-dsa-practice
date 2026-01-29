4952cfe226895fd3654393eb7dfafa6214b0ed61
alokmavi <alokkumarmavi007@gmail.com>
2026-01-29 22:19:20 +0530

    Palindrome String

---
+++ strings/PalindromeString.java
@@ -0,0 +1,29 @@
+package strings;
+
+public class PalindromeString {
+
+    public static boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "madam";
+
+        if (isPalindrome(s)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
