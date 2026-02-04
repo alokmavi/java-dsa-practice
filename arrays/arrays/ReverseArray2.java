310fa30df1a3cfea4c15584d73eff76007489130
alokmavi <alokkumarmavi007@gmail.com>
2026-02-05 00:01:07 +0530

    Reverse array 2nd methode

---
+++ arrays/arrays/ReverseArray2.java
@@ -0,0 +1,24 @@
+package arrays;
+
+public class ReverseArray2 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        System.out.print("Reversed array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
