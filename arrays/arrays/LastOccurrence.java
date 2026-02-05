f95ab6169d66ca8688c7ee081800e4fac360bf5d
alokmavi <alokkumarmavi007@gmail.com>
2026-02-05 22:18:41 +0530

    Binary Search (Occurence)

---
+++ arrays/arrays/LastOccurrence.java
@@ -0,0 +1,26 @@
+package arrays;
+
+public class LastOccurrence {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 2, 3, 4};
+        int target = 2;
+
+        int low = 0, high = arr.length - 1;
+        int result = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                result = mid;
+                low = mid + 1;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        System.out.println("Last occurrence at index: " + result);
+    }
+}
