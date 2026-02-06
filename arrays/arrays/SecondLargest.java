d4a47ec8df6579ef129fff9fd48bf9c92343e4c7
alokmavi <alokkumarmavi007@gmail.com>
2026-02-06 23:41:12 +0530

    Second Largest Element in Array

---
+++ arrays/arrays/SecondLargest.java
@@ -0,0 +1,21 @@
+package arrays;
+
+public class SecondLargest {
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 8, 20, 15};
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+
+        System.out.println("Second largest element: " + secondLargest);
+    }
+}
