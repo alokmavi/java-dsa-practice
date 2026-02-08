18aa1bcdfd1bab68d3c7ffef3e667a1f18c841a1
alokmavi <alokkumarmavi007@gmail.com>
2026-02-08 23:58:53 +0530

    Count Distinct Elements

---
+++ arrays/arrays/CountDistinct.java
@@ -0,0 +1,17 @@
+package arrays;
+
+import java.util.HashSet;
+
+public class CountDistinct {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 4, 4, 5};
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : arr) {
+            set.add(num);
+        }
+
+        System.out.println("Distinct elements count: " + set.size());
+    }
+}
