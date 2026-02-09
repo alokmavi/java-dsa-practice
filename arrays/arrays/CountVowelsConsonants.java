2f928c6c98a4608bcab1f2e168f2e3418ec0b9a9
alokmavi <alokkumarmavi007@gmail.com>
2026-02-09 21:15:43 +0530

    Count Vowels and Consonants

---
+++ arrays/arrays/CountVowelsConsonants.java
@@ -0,0 +1,25 @@
+package arrays;
+
+public class CountVowelsConsonants {
+    public static void main(String[] args) {
+        String s = "interview";
+        int vowels = 0, consonants = 0;
+
+        s = s.toLowerCase();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            }
+        }
+
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+    }
+}
