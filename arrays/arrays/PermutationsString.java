8a0a97ca2e363c96d4f8344ef9b49b4624f45fbe
alokmavi <alokkumarmavi007@gmail.com>
2026-02-14 23:57:53 +0530

    Permutations of a String

---
+++ arrays/arrays/PermutationsString.java
@@ -0,0 +1,22 @@
+package arrays;
+
+public class PermutationsString {
+
+    public static void permute(String s, String ans) {
+        if (s.length() == 0) {
+            System.out.println(ans);
+            return;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            String left = s.substring(0, i);
+            String right = s.substring(i + 1);
+            permute(left + right, ans + ch);
+        }
+    }
+
+    public static void main(String[] args) {
+        permute("abc", "");
+    }
+}
