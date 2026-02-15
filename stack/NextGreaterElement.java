e24bbefcd63f3135e33afbc6ac62c582e95b6037
alokmavi <alokkumarmavi007@gmail.com>
2026-02-15 22:36:33 +0530

    Next Greater Element

---
+++ stack/NextGreaterElement.java
@@ -0,0 +1,32 @@
+package stack;
+
+import java.util.*;
+
+public class NextGreaterElement {
+
+    public static int[] findNGE(int[] arr) {
+        int n = arr.length;
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                stack.pop();
+            }
+
+            result[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(arr[i]);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 2, 10};
+        int[] result = findNGE(arr);
+
+        System.out.println(Arrays.toString(result));
+        // Output: [5, 10, 10, -1]
+    }
+}
